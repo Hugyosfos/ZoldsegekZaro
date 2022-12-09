df3e25fc9dfc8457a737cfab22626a9fd37b64c5
User <User@DESKTOP-6MKR4L0>
2022-12-09 23:58:06 +0100

    Add project files.

---
+++ gyumolcsoktobbform/Zoldsegek.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ZoldsegekZaro
+{
+    internal class Zoldsegek
+    {
+        int id;
+        string nev;
+        double Egyseg;
+        double Mennyiseg;
+
+     
+
+        public int Id { get => id; set => id = value; }
+        public string Nev { get => nev; set => nev = value; }
+        public double Egyseg1 { get => Egyseg; set => Egyseg = value; }
+        public double Mennyiseg1 { get => Mennyiseg; set => Mennyiseg = value; }
+
+        public Zoldsegek(int id, string nev, double egyseg1, double meret1)
+        {
+            Id = id;
+            Nev = nev;
+            Egyseg1 = egyseg1;
+            Mennyiseg1 = Mennyiseg;
+        }
+        public override string ToString()
+        {
+            return "Név:"+ nev + " Egységár:" + Egyseg+" Mennyiség:"+ Mennyiseg;
+        }
+    }
+}
